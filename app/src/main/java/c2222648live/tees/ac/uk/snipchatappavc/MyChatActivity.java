da94ccd5d974d802a9d56ce5845408b21d0d1dff
07495810634 <samueluankhoba49@gmail.com>
2023-04-28 07:12:27 -0700

    finish

--- app/src/main/java/c2222648live/tees/ac/uk/snipchatappavc/MyChatActivity.java
+++ app/src/main/java/c2222648live/tees/ac/uk/snipchatappavc/MyChatActivity.java
@@ -1,7 +1,9 @@
 package c2222648live.tees.ac.uk.snipchatappavc;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -14,10 +16,15 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MyChatActivity extends AppCompatActivity {
@@ -33,6 +40,9 @@ public class MyChatActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference reference;
 
+    MessageAdapter adapter;
+    List<ModelClass> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +53,9 @@ public class MyChatActivity extends AppCompatActivity {
         editTextMessage = findViewById(R.id.editTextMessage);
         fab = findViewById(R.id.fab);
         rvChat = findViewById(R.id.rvChat);
+
+        rvChat.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
 
         database = FirebaseDatabase.getInstance();
         reference = database.getReference();
@@ -70,6 +83,8 @@ public class MyChatActivity extends AppCompatActivity {
                 }
             }
         });
+
+        getMessage();
     }
 
     public void sendMessage(String message)
@@ -87,5 +102,43 @@ public class MyChatActivity extends AppCompatActivity {
                }
             }
         });
+    }
+
+    public void getMessage()
+    {
+        reference.child("Messages").child(userName).child(otherName)
+                .addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ModelClass modelClass = snapshot.getValue(ModelClass.class);
+                list.add(modelClass);
+                adapter.notifyDataSetChanged();
+                rvChat.scrollToPosition(list.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        adapter = new MessageAdapter(list,userName);
+        rvChat.setAdapter(adapter);
+
     }
 }
